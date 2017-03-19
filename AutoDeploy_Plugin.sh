0f4cd18121aaed97d8150b71eb596e873695d8cc
ZFly <466685655@qq.com>
2017-03-19 23:27:41 +0800

    Create AutoDeploy_Plugin.sh
    将自动部署的功能改为插件安装

---
+++ AutoDeploy_Plugin.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+INSTALL_DIR=/usr/local/sMattress
+
+curl -sSL https://github.com/sMattress/Servers_Deploy_Script/AutoDeploy_WebServer.sh > "&INSTALL_DIR"/WebServer/AutoDeploy.sh
+curl -sSL https://github.com/sMattress/Servers_Deploy_Script/AutoDeploy_TcpServer.sh > "&INSTALL_DIR"/Tcp/AutoDeploy.sh
+curl -sSL https://github.com/sMattress/Servers_Deploy_Script/HookServer.rb > "&INSTALL_DIR"/HookServer/HookServer.rb
