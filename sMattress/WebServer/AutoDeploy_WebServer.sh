2a3ea311572f7d492b668d1c9553754ec594c156
ZFly <466685655@qq.com>
2017-03-19 23:34:21 +0800

    Delete AutoDeploy_WebServer.sh
    移除自动部署功能的集成，使用插件的方式安装

--- sMattress/WebServer/AutoDeploy_WebServer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DOCKER_COMPOSE_DIR=/usr/local/sMattress/WebServer/docker-compose.yml
-
-docker-compose -f "$DOCKER_COMPOSE_DIR" pull sMattress_WebServer
-docker-compose -f "$DOCKER_COMPOSE_DIR" stop sMattress_WebServer
-docker-compose -f "$DOCKER_COMPOSE_DIR" up -d sMattress_WebServer
