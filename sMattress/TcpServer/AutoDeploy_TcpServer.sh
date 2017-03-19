bbb56aaf22daca8220002ed633213b499e4d1835
ZFly <466685655@qq.com>
2017-03-19 23:34:07 +0800

    Delete AutoDeploy_TcpServer.sh
    移除自动部署功能的集成，使用插件的方式安装

--- sMattress/TcpServer/AutoDeploy_TcpServer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DOCKER_COMPOSE_DIR=/usr/local/sMattress/TcpServer/docker-compose.yml
-
-docker-compose -f "$DOCKER_COMPOSE_DIR" pull sMattress_TcpServer
-docker-compose -f "$DOCKER_COMPOSE_DIR" stop sMattress_TcpServer
-docker-compose -f "$DOCKER_COMPOSE_DIR" up -d sMattress_TcpServer
