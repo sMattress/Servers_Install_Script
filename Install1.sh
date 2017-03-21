e650ac787c9ee3a016e4b03ac37d2eb776980b75
ZOutstanding <zhang.fly@hotmail.com>
2017-03-21 10:15:09 +0800

    fix bug

---
+++ Install1.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+curl -L http://static.lesmarthome.com/deploy/sMattress.zip > /usr/local/sMattress.zip
+cd /usr/local
+unzip /usr/local/sMattress.zip
+
+rm -rf __MACOSX
+rm sMattress.zip
