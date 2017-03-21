5468d1d6674a1ff213d92b9f344bb12161e74e0f
ZOutstanding <zhang.fly@hotmail.com>
2017-03-21 13:04:32 +0800

    fix bug

--- AutoDeploy_Plugin.sh
+++ AutoDeploy_Plugin.sh
@@ -5,4 +5,5 @@ REPOSITORY_DIR=https://raw.githubusercontent.com/sMattress/Servers_AutoDeploy_Sc
 
 curl "$REPOSITORY_DIR"/AutoDeploy_WebServer.sh > "$INSTALL_DIR"/WebServer/AutoDeploy.sh
 curl "$REPOSITORY_DIR"/AutoDeploy_TcpServer.sh > "$INSTALL_DIR"/TcpServer/AutoDeploy.sh
+mkdir "$INSTALL_DIR"/HookServer
 curl "$REPOSITORY_DIR"/HookServer.rb > "$INSTALL_DIR"/HookServer/HookServer.rb
