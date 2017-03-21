126080c79e1e3d11f83bc4c3893e5b4a5f47ab98
ZOutstanding <zhang.fly@hotmail.com>
2017-03-21 12:57:46 +0800

    fix bug

--- Install.sh
+++ Install.sh
@@ -2,7 +2,7 @@
 
 curl -sSL http://acs-public-mirror.oss-cn-hangzhou.aliyuncs.com/docker-engine/internet | sh -
 sudo systemctl start docker
-curl -L http://omdthatdk.bkt.clouddn.com/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose
+curl http://omdthatdk.bkt.clouddn.com/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 curl https://raw.githubusercontent.com/sMattress/Servers_Install_Script/master/sMattress.zip > /usr/local/sMattress.zip
 cd /usr/local
